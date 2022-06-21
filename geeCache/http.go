9653b05ce657f3d41d4ff64ffe4a02d0da335648
hq <1422127065@qq.com>
2022-06-22 00:03:13 +0800

    http 服务端测试 结束

--- geeCache/http.go
+++ geeCache/http.go
@@ -22,18 +22,21 @@ func NewHttpPool(self string) *HttpPool {
 }
 
 func (p *HttpPool) Log(format string, v ...interface{}) {
-	log.Println("[Server %s] %s", p.self, fmt.Sprintf(format, v))
+	log.Printf("[Server %s] %s\n", p.self, fmt.Sprintf(format, v))
 }
 
-func (p *HttpPool) ServerHttp(w http.ResponseWriter, r *http.Request) {
+func (p *HttpPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if strings.Contains(r.URL.Path, "favicon") {
+		return
+	}
 	if !strings.HasPrefix(r.URL.Path, p.basePath) {
 		panic("HTTPPool serving unexpected path: " + r.URL.Path)
 	}
 
-	p.Log("%s %s", r.Method, r.URL.Path)
+	p.Log(r.Method, r.URL.Path)
 	// /<basepath>/<groupname>/<key> required
 	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
-
+	fmt.Println(parts)
 	if len(parts) != 2 {
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
@@ -42,7 +45,18 @@ func (p *HttpPool) ServerHttp(w http.ResponseWriter, r *http.Request) {
 	groupName := parts[0]
 	key := parts[1]
 
-	groupName := GetGroup(groupName)
-	key := parts[1]
+	group := GetGroup(groupName)
 
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+	//w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
 }
